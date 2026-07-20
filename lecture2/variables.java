941ab751204bcf62dc0710e029586410e2bf548d
aayushgupta52 <iguptaaayush09@gmail.com>
2026-07-20 23:01:07 +0530

    Add lecture1 and lecture2

---
+++ lecture2/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        // int firstNumber = 20;
+        // int secondNumber = 80;
+        // int total = firstNumber - secondNumber;
+        // char grade = 'A';
+        boolean isStudent = true;
+        System.out.println(isStudent);
+    }
+
+}
