1422f2f905936341d4e4f6e487f8496590d4c4ce
aayushgupta52 <iguptaaayush09@gmail.com>
2026-07-22 15:11:55 +0530

    Add Lecture 5: Java Input and Output using Scanner

---
+++ lecture5/inputoutput.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class inputoutput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the first num1 : ");
+        // int num1 = sc.nextInt();
+        // System.out.println("Enter the first num2 : ");
+        // int num2 = sc.nextInt();
+        // int num3 = num1 + num2;
+        // System.out.println("Addition of num1 and num 2 is  :" + num3);
+
+
+
+        // System.out.println("Enter the bg : ");
+        // BigInteger bg = sc.nextBigInteger();
+        // System.out.println("BigInteger: " + bg);
+
+        // System.out.println("Enter the flag : ");
+        // boolean flag = sc.nextBoolean();
+        // System.out.println("value of the flag : " + flag);
+
+        // System.out.println("Enter the  shortVal : ");
+        // short shortVal = sc.nextShort();
+        // System.out.println("Enter the  value of shortVal:" + shortVal);
+
+
+
+        System.out.println("Enter the  floatValue: ");
+        float floatValue = sc.nextFloat();
+        System.out.println("Enter the  value of floatValue :" + floatValue);
+
+        sc.close();
+
+
+
+
+
+        
+
+
+    }
+}
