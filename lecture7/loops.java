c801b307c423f64ce52063f8b99784aa1119a605
aayushgupta52 <iguptaaayush09@gmail.com>
2026-07-25 22:29:59 +0530

    Add Lecture 7: Java Loops

---
+++ lecture7/loops.java
@@ -0,0 +1,104 @@
+// public class loops {
+//     public static void main(String[] args) {
+//         // for(int i = 1 ; i<= 10 ; i += 2){
+//         // System.out.println(i);
+//         // }
+
+
+//            for(int i = 2 ; i<= 10 ; i += 2){
+//         System.out.println(i);
+//            }
+//     }
+// }
+
+
+// public class loops {
+//     public static void main(String[] args) {
+//         for(int i = 1 ; i<= 4 ; i++){
+//             for(int j = 1 ; j <= 4 ; j++) { 
+//                 System.out.print("*");
+//             }
+//              System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class loops {
+//     public static void main(String[] args) {
+//         for(int i = 1 ; i <= 4 ; i++) {
+//             for(int j= 1 ; j <= 4 ; j++){
+//             System.err.println("i =   "   +   i +   "j =   "  +   j );
+//             }
+    
+//         }
+//     }
+// }
+
+
+
+// break statement 
+// public class loops {
+//     public static void main(String[] args) {
+//         for(int i=1 ; i<= 10 ; i++){
+//             if(i== 4){
+//             break;
+//         }
+//         System.out.println(i);
+//     }
+//     }
+// }
+
+
+
+// continue statment 
+
+
+// public class loops {
+//     public static void main(String[] args) {
+//         for(int i = 1 ; i <= 10 ; i++){
+//             if(i== 5){
+//                 continue;
+//             }
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+//  while loops  
+
+// public class loops {
+//    public static void main(String[] args) {
+//     int i = 1;
+//     while(i <= 5) {
+//         System.out.println(i);
+//         i++;
+//     }
+//    }
+// }
+
+
+
+
+
+// do while loop 
+
+
+
+
+// ek barr to hchalega kese bih karke 
+
+// public class loops {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         do { 
+//             System.out.println(i);
+//            i++;
+//         } while (i<=0);
+//     }
+// }
+
+
+
