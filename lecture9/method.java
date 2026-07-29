d7dcd5e8b8757ec474445d3814b9f50b9ac50bd6
aayushgupta52 <iguptaaayush09@gmail.com>
2026-07-29 23:14:04 +0530

    Add Lecture 9: Java Methods

---
+++ lecture9/method.java
@@ -0,0 +1,30 @@
+
+// //declaration / definition  
+// public class method {
+//     static void print2KaTable() {
+//         for(int i = 1 ; i <= 10 ; i++) {
+//             int ans = i * 2;
+//             System.out.println( "--> " + ans);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         System.out.println("hi");
+//         print2KaTable();
+//         System.out.println("Bye");
+//     }
+// }
+
+
+// parameter ke saat 
+
+public class method {
+     static void add2Sum(int x , int y) {
+        int z = x + y;
+        System.out.println(z);
+
+    }
+    public static void main(String[] args) {
+        add2Sum( 10 , 20);
+        
+    }
+}
