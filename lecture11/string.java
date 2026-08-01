675d65eb8482e12429c1ebead77903480c8522e6
aayushgupta52 <iguptaaayush09@gmail.com>
2026-08-01 23:34:55 +0530

    Add Lecture 11: Java Strings

---
+++ lecture11/string.java
@@ -0,0 +1,236 @@
+// // public class string {
+// //     public static void main(String[] args) {
+// //         String firstName = "Aayush";
+// //         String lastName = new String ("Gupta");
+//         System.out.println(" Full name of the candidates :" + " " + firstName +  " " +  lastName );
+// //         System.out.println(firstName.length());
+// //         System.out.println(firstName.charAt(5));
+        
+// //     }
+
+// // }
+
+
+
+//immutable string 
+
+// public class string {
+//         public static void main(String[] args) {
+//                 String Name = "Aayush";
+//                  Name = "Piyush";
+//                 System.out.println(Name);
+
+//         }
+// }
+
+
+
+// comparing string 
+
+// public class string {
+//         public static void main(String[] args) {
+//             String name1 = "Aayush";
+//             String name2 = "Aayush";
+//             if(name1 == name2) {
+//                 System.out.println("print thr name : ");
+//             }
+//             else {
+//                 System.out.println("incorrect values : ");
+//             }
+//         }
+// }
+
+
+// public class string {
+//         public static void main(String[] args) {
+//             String name1 = "Aayush";
+//             String name2 = "Aayush";
+//             if(name1.equals(name2)) {
+//                 System.out.println("print the values:");
+//             }
+//             else {
+//                 System.out.println("dont print the values:");
+//             }
+//         }
+// }
+
+
+
+
+// public class string {
+//         public static void main(String[] args) {
+//             String name1 = "Aayush";
+//             String name2 = "aayush";
+//             if(name1.equalsIgnoreCase(name2)) {
+//                 System.out.println("print the values:");
+//             }
+//             else {
+//                 System.out.println("dont print the values:");
+//             }
+//         }
+// }
+
+
+
+
+
+
+// sc.Next()
+
+// import java.util.Scanner;
+// public class string {
+//         public static void main(String[] args) {
+//         System.out.print("Provide the values:");
+//             Scanner sc =  new Scanner(System.in);
+//             String name = sc.next();
+//             System.out.println(name);
+//         }
+// }
+
+
+// import java.util.Scanner;
+// public class string {
+//         public static void main(String[] args) {
+//         System.out.print("Provide the values:");
+//             Scanner sc =  new Scanner(System.in);
+//             String name = sc.nextLine();
+//             System.out.println(name);
+//         }
+// }
+
+
+// public class string {
+//         public static void main(String[] args) {
+            
+        // empty -> length = 0
+        // blank -> empty or sirf spaces h string
+
+        // String str = " ";
+
+        // System.out.println(str.length());
+        // System.out.println(str.isEmpty());
+        // System.out.println(str.isBlank());
+
+        // String name = "   Love   ";
+
+        // System.out.println(name.length());
+
+        // name = name.trim();
+
+        // System.out.println(name.length());
+
+        // String name = "aayush";
+        // System.out.println(name.toUpperCase());
+
+        //  String name1 = "AAYUSH";
+        // System.out.println(name.toLowerCase());
+
+        // String str = "My Name is Love Babbar";
+
+        // System.out.println(str.substring(0, 7));
+
+        // String str = "Aayush Gupta";
+
+        // System.out.println(str.substring(0, 6));
+
+
+
+        // String str = "My Name is Love Babbar";
+
+        // System.out.println(str.contains("Java"));
+
+        // int num = 5123;
+        // String str = String.valueOf(num);
+        // int value = num + 1 ;
+        // System.out.println(str + 1);
+        // System.out.println(value);
+
+
+        // String name = "My name is Aayush";
+        // System.out.println(name.startsWith("Aayush"));
+        // System.out.println(name.endsWith("Aayush"));
+
+
+
+        // String input = "My,name,is,Love,Babbar";
+
+        // String[] words = input.split(",");
+
+        // for (String str : words) {
+        //     System.out.println(str);
+        // }
+            
+
+
+        // String name = "Babbar";
+
+        // char[] arr = name.toCharArray();
+
+        // for (char ch : arr) {
+        //     System.out.println(ch);
+
+
+
+//         String name = "babbar";
+
+//         name = name.replace('b', 'g');
+
+//         System.out.println(name);
+
+//     }
+// }
+
+
+
+// public class string {
+
+//     static void printString(String str){
+//         int n = str.length();
+//         for (int i = 0; i < n; i++) {
+//             char ch = str.charAt(i);
+//             System.out.println(ch);
+//         }
+//     }
+//     public static void main(String[] args) {
+//             String  str = "Aayush";
+//         printString(str);
+       
+//     }
+// }
+
+
+
+// count vowels in the string 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
