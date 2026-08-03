a02f2242de5383183ec34ef5de76c75f9265f9e7
aayushgupta52 <iguptaaayush09@gmail.com>
2026-08-03 23:21:54 +0530

    Add Lecture 12: Java Math Problems

---
+++ lecture12/math.java
@@ -0,0 +1,32 @@
+//  print digits of number 
+
+
+// public class math {
+//       static void printDigit(int num) {
+//         while(num!= 0) {
+//             int digit = num % 10;
+//             System.out.println(digit); // last value print 
+//              num = num/10;
+//         }
+//         }
+//     public static void main(String[] args) {
+//         int num = 5173;
+//         printDigit(num);
+
+   
+
+//     }
+
+// }
+
+
+// count the digit 
+
+public class math {
+    static void count(int num) {
+        while(num )
+    }
+    public static void main(String[] args) {
+        
+    }
+}
