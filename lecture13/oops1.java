fe2330b7b33a9fbe1c2fb59c8462bc9f1ab9e4a5
aayushgupta52 <iguptaaayush09@gmail.com>
2026-08-04 23:39:34 +0530

    intro to oops

---
+++ lecture13/oops1.java
@@ -0,0 +1,67 @@
+// public class oops1 {
+//     // Attributes (Data Members)
+//      String name;
+//      int age;
+//      int id;
+
+
+//      //Behaviour (Method)
+
+//      void study() {
+//         System.out.println(name + "is studying");
+//      }
+
+//      void sleep() {
+//         System.out.println( name  + "is sleeping");
+//      }
+//      void bunk() {
+//         System.out.println( name  + "bunked the class");
+//      }
+
+//      void height() {
+//         System.out.println( age + "total age");
+
+//      }
+
+//      void branch() {
+//         System.out.println(id + "class id or branch");
+//      }
+
+
+
+
+//     public static void main(String[] args) {
+        
+//     }
+// }
+
+
+// public class oops1 {
+//     public static void main(String[] args) {
+        
+
+//         // object creation 
+
+//         Student s1 = new Student();
+
+//         //Assign values 
+//         s1.name = "Lucky";
+//         s1.age = 18;
+//         s1.id = 1234;
+
+
+//          // Access Data
+//         System.out.println(s1.name);
+//         System.out.println(s1.age);
+//         System.out.println(s1.id);
+
+
+//      // call method 
+
+//      s1.study();
+//      s1.sleep();
+//      s1.bunk();
+//     }
+// }
+
+
